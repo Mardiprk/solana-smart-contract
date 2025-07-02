6e6037180a7c393ae633f5c169ed6433b5f0e748
Mardiprk <mk1659770@gmail.com>
2025-07-02 04:33:03 +0000

    client side

--- tests/solana-smart-contract.ts
+++ tests/solana-smart-contract.ts
@@ -2,6 +2,17 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaSmartContract } from "../target/types/solana_smart_contract";
 
-describe("solana-smart-contract", ()=>{
+describe("solana-smart-contract", () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
+  const program = anchor.workspace.SolanaSmartContract as Program<SolanaSmartContract>;
+
+  it("Initialize with a message", async () =>{
+    const dataAccount = anchor.web3.Keypair.generate();
+
+    await program.methods.initialize("Hello, SOL").accounts({
+
+    });
+  });
 });
