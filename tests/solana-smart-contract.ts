84b3710ab144fbf6f876dfa67c0704788032b220
Mardiprk <mk1659770@gmail.com>
2025-07-02 10:31:06 +0000

    client side

--- tests/solana-smart-contract.ts
+++ tests/solana-smart-contract.ts
@@ -1,18 +1,23 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { SolanaSmartContract } from "../target/types/solana_smart_contract";
+import {SolanaSmartContract} from '../target/types/solana_smart_contract';
 
-describe("solana-smart-contract", () => {
+describe("solana-smart-contract", () =>{
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
 
   const program = anchor.workspace.SolanaSmartContract as Program<SolanaSmartContract>;
 
-  it("Initialize with a message", async () =>{
+  it("initialize the test", async () =>{
     const dataAccount = anchor.web3.Keypair.generate();
 
-    await program.methods.initialize("Hello, SOL").accounts({
+    await program.methods.initialize("Hello, SOLANA").accounts({
+      data: dataAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    }).signers([dataAccount]).rpc();
 
-    });
+    const account = await program.account.dataAccount.fetch(dataAccount.publicKey);
+    console.log("Stored Message: ", account.message);
   });
 });
