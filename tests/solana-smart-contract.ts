958b1fcccda978da120e15b7ec087f23b6fa314a
Mardiprk <mk1659770@gmail.com>
2025-07-08 18:41:41 +0000

    SOL logs and events

--- tests/solana-smart-contract.ts
+++ tests/solana-smart-contract.ts
@@ -3,12 +3,23 @@ import { Program } from "@coral-xyz/anchor";
 import {Sysvar} from "../target/types/sysvar";
 
 describe("sysvar", () => {
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Sysvar as Program<Sysvar>;
 
-  it("Initialize", async () =>{
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your Transaction signature: {}", tx);
+  it("initializes and read events", async () => {
+    const txSig = await program.methods.initialize().rpc();
+    console.log("Tx Signature:", txSig);
+
+    const tx = await provider.connection.getTransaction(txSig,{
+      commitment:"confirmed",
+      maxSupportedTransactionVersion: 0,
+    });
+
+    const logs = tx?.meta?.logMessages ?? [];
+    console.log("Program Logs:");
+    logs.forEach(log => console.log(log));
   });
 });
