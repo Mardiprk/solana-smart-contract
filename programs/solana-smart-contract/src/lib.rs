b90c4eb5fb5bd17e034d5bb4fb8bcf3f4efc54be
Mardiprk <mk1659770@gmail.com>
2025-07-09 17:46:51 +0000

    Compute unit

--- programs/solana-smart-contract/src/lib.rs
+++ programs/solana-smart-contract/src/lib.rs
@@ -3,18 +3,13 @@ use anchor_lang::prelude::*;
 declare_id!("FXF4PruD9YkoGuiGbRD8yAQSShtQDeYrhYYWZy4Nm7bS");
 
 #[program]
-pub mod day14 {
+pub mod compute_unit {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        let signer = &mut ctx.accounts.signer;
-        msg!("The Signer is {:?}", *signer.key);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(mut)]
-    pub signer: Signer<'info>,
-}
+pub struct Initialize {}
--- tests/solana-smart-contract.ts
+++ tests/solana-smart-contract.ts
@@ -1,18 +1,28 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import {Day14} from "../target/types/day14";
+import {ComputeUnit} from "../target/types/compute_unit";
 
-describe("Day14", () => {
+describe("compute_unit", ()=>{
   anchor.setProvider(anchor.AnchorProvider.env());
-  const program = anchor.workspace.Day14 as Program<Day14>;
+  const program = anchor.workspace.ComputeUnit as Program<ComputeUnit>;
 
-  it("checking signer",async () =>{
-    const tx = await program.methods.initialize().accounts({
-      signer: program.provider.publicKey
-    }).rpc();
+  const defaultKeypair = new anchor.web3.PublicKey(
+    "5NpeRLioZMTEZ6pcAtCvH3LQ9RaYmBBSu48Y4EXmkuXg"
+  );
 
-    console.log("Your Tx Signature: ",tx);
-    console.log("The signer: ", program.provider.publicKey.toBase58());
+  it("initialized", async () =>{
+
+    let bal_before = await program.provider.connection.getBalance(defaultKeypair);
+    console.log("Bal. before: ", bal_before);
+
+    let tx = await program.methods.initialize().rpc();
+
+    let bal_after = await program.provider.connection.getBalance(defaultKeypair);
+    console.log("Bal. After: ", bal_after);
+
+    console.log("Difference:", BigInt(bal_before.toString()) - BigInt(bal_after.toString())); 
   });
 });
-  
+
+//checking the CU for SOLANA Programs and it came out 5000 Lamports for this single instructions
+//which is the baseline transaction fee
