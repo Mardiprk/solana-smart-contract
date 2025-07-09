c8512ade70a27b7749379161c477f009162e4e35
Mardiprk <mk1659770@gmail.com>
2025-07-09 06:36:07 +0000

    client

--- tests/solana-smart-contract.ts
+++ tests/solana-smart-contract.ts
@@ -6,15 +6,13 @@ describe("Day14", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
   const program = anchor.workspace.Day14 as Program<Day14>;
 
-  it("signer checker", async () => {
-    const tx = await program.methods
-      .initialize()
-      .accounts({
-        signer: program.provider.publicKey,
-      })
-      .rpc();
-    console.log("he Signer: ", program.provider.publicKey.toBase58());
-    console.log("TX Signature: ", tx);
+  it("checking signer",async () =>{
+    const tx = await program.methods.initialize().accounts({
+      signer: program.provider.publicKey
+    }).rpc();
+
+    console.log("Your Tx Signature: ",tx);
+    console.log("The signer: ", program.provider.publicKey.toBase58());
   });
 });
   
