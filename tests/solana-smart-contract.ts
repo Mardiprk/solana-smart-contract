f33b04a3c0623d0dd00f3c8a736290a4b4222bce
Mardiprk <mk1659770@gmail.com>
2025-07-10 06:47:15 +0000

    CU

--- tests/solana-smart-contract.ts
+++ tests/solana-smart-contract.ts
@@ -4,25 +4,21 @@ import {ComputeUnit} from "../target/types/compute_unit";
 
 describe("compute_unit", ()=>{
   anchor.setProvider(anchor.AnchorProvider.env());
+
   const program = anchor.workspace.ComputeUnit as Program<ComputeUnit>;
 
-  const defaultKeypair = new anchor.web3.PublicKey(
-    "5NpeRLioZMTEZ6pcAtCvH3LQ9RaYmBBSu48Y4EXmkuXg"
-  );
+  const defaultKeypair = new anchor.web3.PublicKey("5NpeRLioZMTEZ6pcAtCvH3LQ9RaYmBBSu48Y4EXmkuXg");
 
-  it("initialized", async () =>{
+  it("Compute Unit", async () => {
+    const bal_before = await program.provider.connection.getBalance(defaultKeypair);
+    console.log("Bal. Before:", bal_before);
 
-    let bal_before = await program.provider.connection.getBalance(defaultKeypair);
-    console.log("Bal. before: ", bal_before);
+    const tx = await program.methods.initialize().rpc();
 
-    let tx = await program.methods.initialize().rpc();
-
-    let bal_after = await program.provider.connection.getBalance(defaultKeypair);
+    const bal_after = await program.provider.connection.getBalance(defaultKeypair);
     console.log("Bal. After: ", bal_after);
+    console.log("Difference", BigInt(bal_before.toString()) - BigInt(bal_after.toString()));
 
-    console.log("Difference:", BigInt(bal_before.toString()) - BigInt(bal_after.toString())); 
+    console.log("TX Signature: ", tx);
   });
 });
-
-//checking the CU for SOLANA Programs and it came out 5000 Lamports for this single instructions
-//which is the baseline transaction fee
